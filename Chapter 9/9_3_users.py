66b8a3b334b669092e79eef7701e0a93e5234f3c
Jordan Gibbard <46365880+Varsonix@users.noreply.github.com>
2025-05-05 20:11:00 -0700

    updates to users class

--- Chapter 9/9_3_users.py
+++ Chapter 9/9_3_users.py
@@ -3,6 +3,7 @@ class User:
     def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
+        self.login_attempts = 0
     
     def describe_user(self):
         print(
@@ -13,6 +14,14 @@ class User:
     def greet_user(self):
         full_name = f"{self.first_name} {self.last_name}"
         print(f"\nGreetings, {full_name.title()}!\n")
+    
+    def increment_login_attempts(self):
+        """ Increment Login Attempts attribute. """
+        self.login_attempts += 1
+    
+    def reset_login_attempts(self):
+        """ Reset login attempts attribute. """
+        self.login_attempts = 0
 
 jordan = User('jordan', 'gibbard')
 robert = User('robert', 'randalf')
@@ -26,3 +35,11 @@ robert.greet_user()
 
 michael.describe_user()
 michael.greet_user()
+
+new_user = User('Bob', 'Dole')
+new_user.describe_user()
+print(f"Incrementing Login Attempts by 1")
+new_user.increment_login_attempts()
+print(f"Login attempts is now {new_user.login_attempts}. Now resetting...")
+new_user.reset_login_attempts()
+print(f"Login attempts is now {new_user.login_attempts}")
