21bc30a3e0d7e96ba641dcb99494566df9b29268
Jordan Gibbard <46365880+Varsonix@users.noreply.github.com>
2025-05-05 20:06:16 -0700

    Updates

--- Chapter 9/9_1_restaurant.py
+++ Chapter 9/9_1_restaurant.py
@@ -5,26 +5,30 @@ class Restaurant:
         """ Initialize the name and type attributes"""
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
+        self.number_served = 0
     
     def describe_restaurant(self):
         print(
             f"{self.restaurant_name} is a {self.cuisine_type} based "
-            "restaurant operating out of San Marcos."
+            "restaurant operating out of San Marcos "
+            f"and has served {self.number_served} customers."
         )
 
     
     def open_restaurant(self):
         print("The restaurant is now open!\n")
+    
+    def set_number_served(self, customers):
+        """Update the number of customers served."""
+        self.number_served = customers
+    
+    def increment_number_served(self, customers):
+        """Increment customers"""
+        self.number_served += customers
 
-applebees = Restaurant("Applebee's Bar and Grill", "Neighborhood")
-chilis = Restaurant("Chili's Bar and Grill", "Baby Back Ribs")
-mcdonalds = Restaurant("McDonalds", "Fast Food")
-print('\n')
+applebees = Restaurant('Applebees Bar and Grill', 'Bad Food')
 applebees.describe_restaurant()
-applebees.open_restaurant()
-
-chilis.describe_restaurant()
-chilis.open_restaurant()
-
-mcdonalds.describe_restaurant()
-mcdonalds.open_restaurant()
+applebees.set_number_served(33)
+applebees.describe_restaurant()
+applebees.increment_number_served(7)
+applebees.describe_restaurant()
